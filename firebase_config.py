92105d2cfced10ac635a03c098d1c73ff2f59cef
Dongseon Yang <ydsperfect08@ewhain.net>
2025-07-14 04:07:01 +0900

    Feat : Render 배포 오류 해결

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,19 @@
-# firebase_config.py
-
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# 서비스 계정 키 경로
-cred = credentials.Certificate("firebase-key.json")
+# 환경변수에서 JSON 문자열 가져오기
+firebase_key_json = os.environ.get("FIREBASE_KEY_JSON")
 
-# Firebase 앱 초기화
+# JSON 문자열을 파싱해서 dict로 변환
+firebase_key_dict = json.loads(firebase_key_json)
+
+# dict를 credentials로 변환
+cred = credentials.Certificate(firebase_key_dict)
+
+# Firebase 초기화
 firebase_admin.initialize_app(cred)
 
-# Firestore 클라이언트 가져오기
+# Firestore 클라이언트
 db = firestore.client()
